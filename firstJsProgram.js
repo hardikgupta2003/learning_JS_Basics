774cc94c53b6cc768a2833007e12c7cb0cb3dbd2
hardikgupta2003 <94279016+hardikgupta2003@users.noreply.github.com>
2023-06-07 12:44:55 +0530

    Create firstJsProgram.js

---
+++ firstJsProgram.js
@@ -0,0 +1,12 @@
+console.log('heyyyy');
+
+let rectangle={
+    lenght:1,
+    breadth: 2,
+
+    draw : function(){
+        console.log('this is the function of drawing');
+    }
+}
+
+console.log(rectangle.draw());
